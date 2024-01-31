ec20b8b816acc73356e8a0eef4dd20d3c656a49b
Ian Andwati <andwatiian@gmail.com>
2024-01-31 17:39:59 +0300

    mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,71 +6,52 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
-import { Badge } from "@/components/ui/badge";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import ThemeSwitcher from "@/components/theme-switcher";
 import fs from "fs";
 import path from "path";
 import Link from "next/link";
 
-type BiasType = {
+type Bias = {
   name: string;
   description: string;
-  wikipedia_link: string;
-};
-
-type Bias = {
-  bias: string;
-  description: string;
-  wikipedia_link: string;
-  types: BiasType[];
+  link: string;
 };
 
 export default async function Home() {
   const dataBuffer = fs.readFileSync(path.resolve("_data/data.json"));
   const jsonData: Bias[] = JSON.parse(dataBuffer.toString());
+
   return (
     <main>
       <nav className="flex justify-between">
         <h1>Cognitive biases</h1>
         <div>
-          {" "}
           <ThemeSwitcher />
         </div>
       </nav>
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-1  gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
         {jsonData.map((bias, index) => (
-          <>
-            <div key={index}>
-              <h3>{bias.bias}</h3>
+          <Card key={index} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <div>
+                <CardTitle>{bias.name}</CardTitle>
+              </div>
+            </CardHeader>
+
+            <CardContent>
               <p>{bias.description}</p>
-            </div>
-            {bias.types.map((type, i) => {
-              <Card key={i} className="flex flex-col justify-between">
-                <CardHeader className="flex-row gap-4 items-center">
-                  <div>
-                    <CardTitle>{type.name}</CardTitle>
-                    <CardDescription>
-                      Bias Group Link : {bias.wikipedia_link}
-                    </CardDescription>
-                  </div>
-                </CardHeader>
+            </CardContent>
 
-                <CardContent>
-                  <p>{type.description}</p>
-                </CardContent>
-
-                <CardFooter className="flex justify-between">
-                  <Button>
-                    <Link href={type.wikipedia_link} target="_blank">
-                      View on Wikipedia
-                    </Link>
-                  </Button>
-                </CardFooter>
-              </Card>;
-            })}
-          </>
+            <CardFooter className="flex justify-between">
+              <Button>
+                <Link href={bias.link} target="_blank">
+                  View on Wikipedia
+                </Link>
+              </Button>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </main>
