878abab4ed0363f82529b5309b73e33b18b390ac
Ian Andwati <andwatiian@gmail.com>
2024-01-31 19:09:42 +0300

    hmm

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/cognitive-biases",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
